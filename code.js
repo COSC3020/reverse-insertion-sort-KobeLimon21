dbb5040e1c087046052e3e3aa4b94c0096a900c6
KobeLimon21 <kobelimon2121@gmail.com>
2024-09-05 11:31:11 -0600

    Update code.js

--- code.js
+++ code.js
@@ -1,3 +1,17 @@
-function insertionSortReverse(array) {
-    return array;
+function  insertionSortReverse (array){
+for(i = array.length - 2; i >= 0 ; i--){
+    var val = array[i];
+var j;
+for(j = i; j < array.length - 1 && array[j+1] < val; j++) {
+    array[j] = array[j+1];
+  }
+  array[j] = val;
 }
+return array;
+}
+
+
+let arr = [5,2,3,4,7];
+console.log(insertionSortReverse(arr)); 
+    
+
